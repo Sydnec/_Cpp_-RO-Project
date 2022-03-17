b4e157050a2a7f2f45fc13a538dac4217325f065
Sydnec <97565235+Sydnec@users.noreply.github.com>
2022-03-17 14:16:56 +0100

    add RO.h

--- RO.cpp
+++ RO.cpp
@@ -1,44 +1,7 @@
-#include <iostream>
-#include <stdio.h>
-#include <iostream>
-#include <fstream>
-#include <vector>
-#include <string>
-
-using namespace std;
+#include "RO.h"
 
 int main()
 {
-	//B, C, Dq, Eq
-	vector<string> vDir(2);
-	vDir[0] = "B";
-	vDir[1] = "C";
-
-	for (int i(0); i < vDir.size(); ++i) {
-		string dirname = "./BildeKrarup/" + vDir[i] + "/files.lst";
-		
-		ifstream mydir;
-		mydir.open(dirname);
-		if (!mydir.is_open()) cerr << "Can't open file " << dirname << '\n';
-		else {
-			ifstream myfile;
-			string line = "", filename ="";
-
-			while (getline(mydir, line)) {
-				string fileLine = "";
-				filename = "./BildeKrarup/" + vDir[i] + '/' + line;
-				cout << "Filename = " << filename << '\n';
-				myfile.open(filename);
-				if (!myfile.is_open()) cerr << "Can't open file " << filename << '\n';
-				
-				while (getline(myfile, fileLine)) {
-					cout << fileLine;
-				}
-				cout << '\n';
-				myfile.close();
-			}
-			mydir.close();
-		}
-	}
-	return 0;
+    // BC();
+    DqEq();
 }
